175bf7b832214571021bb0a3875b87c3e143dde9
Oskar Albers <oalbers@wundermancommerce.com>
2020-11-03 16:09:27 +0100

    fixed test case

--- src/test/java/com/xignitex/demoservice/DemoServiceApplicationTests.java
+++ src/test/java/com/xignitex/demoservice/DemoServiceApplicationTests.java
@@ -1,12 +1,19 @@
 package com.xignitex.demoservice;
 
+import com.xignitex.demoservice.gateway.artistgateway.ArtistRepository;
 import com.xignitex.demoservice.model.Artist;
 import com.xignitex.demoservice.usecase.GetArtist;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
@@ -15,10 +22,19 @@ class DemoServiceApplicationTests {
 	@InjectMocks
 	private GetArtist getArtist;
 
+	@Mock
+	private ArtistRepository artistRepository;
+
 	@Test
 	void shouldGetArtist() {
-
 		final String id = "123";
+
+		Artist test_artist = new Artist();
+		test_artist.setName("test");
+
+		when(artistRepository.findById(eq(id)))
+				.thenReturn(Optional.of(test_artist));
+
 		Artist artist = getArtist.execute(id);
 		assertThat(artist.getName()).isEqualTo("test");
 	}
