062e7b79a17e7d2b032e430a966f29b5d229b8f9
Oskar Albers <oalbers@wundermancommerce.com>
2020-11-03 10:42:17 +0100

    Added test case

--- src/test/java/com/xignitex/demoservice/DemoServiceApplicationTests.java
+++ src/test/java/com/xignitex/demoservice/DemoServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.xignitex.demoservice;
 
+import com.xignitex.demoservice.model.Artist;
+import com.xignitex.demoservice.usecase.GetArtist;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DemoServiceApplicationTests {
 
+	@InjectMocks
+	private GetArtist getArtist;
+
 	@Test
-	void contextLoads() {
+	void shouldGetArtist() {
+
+		final String id = "123";
+		Artist artist = getArtist.execute(id);
+		assertThat(artist.getName()).isEqualTo("test");
 	}
 
 }
